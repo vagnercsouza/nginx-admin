e51b62371939e52776966e14a8d43677155896b9
Vagner Cavalcante Souza <vagner.souza@catho.com>
2016-05-24 18:30:39 -0300

    Added commands and templates

--- run.py
+++ run.py
@@ -3,7 +3,21 @@
 from flask import Flask
 from flask import render_template
 
+import subprocess
+
 app = Flask(__name__)
+
+def run(cmd):
+	p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+	html = ''
+	while True:
+	    out = p.stderr.read(1)
+	    if out == '' and p.poll() != None:
+	        break
+	    if out != '':
+	        html = html + out + '\n'
+
+	return html
 
 @app.route("/")
 def index():
@@ -20,21 +34,29 @@ def nginx_config():
 def create_site():
 	return render_template('create_site.html')
 
+@app.route("/save-site")
+def save_site():
+	return 'Saved'
+
 @app.route("/start-nginx")
 def start_nginx():
-	return render_template('start_nginx.html')
+	output = subprocess.check_output("service nginx start", shell=True)
+	return output.replace('\n', '<br>')
 
 @app.route("/stop-nginx")
 def stop_nginx():
-	return render_template('stop_nginx.html')
+	output = subprocess.check_output("service nginx stop", shell=True)
+	return output.replace('\n', '<br>')
 
 @app.route("/reload-nginx")
 def reload_nginx():
-	return render_template('reload_nginx.html')
+	output = subprocess.check_output("service nginx reload", shell=True)
+	return output.replace('\n', '<br>')
 
 @app.route("/test-nginx")
 def test_nginx():
-	return render_template('test_nginx.html')
+	output = subprocess.check_output("service nginx configtest", shell=True)
+	return output.replace('\n', '<br>')
 
 if __name__ == "__main__":
 	app.run(debug=True)
